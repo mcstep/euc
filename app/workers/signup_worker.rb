5662292d0f1155290bb29ffa69fb30a93f0183c8
Ashutosh Joshi <ashutoshj@ubuntu.(none)>
2014-06-19 15:43:03 -0700

    change order of events to reduce race conditions

--- app/workers/signup_worker.rb
+++ app/workers/signup_worker.rb
@@ -28,18 +28,15 @@ class SignupWorker
 
     puts "Done creating account. Response from AD #{json_body}"
    
-    # Find a way to optimize this
-    domain_name = @invitation.recipient_email.split("@").last
-    @domain = Domain.find_by_name(domain_name)
-    if !@domain.nil? && @domain.status == 'active'
-      puts "Sending Super User email...."
-        WelcomeUserMailer.welcome_email(@invitation,json_body['password'],ENV['DOMAIN']).deliver
-      puts "Email sent successfully"
-    else
-      puts "Sending Regular User email...."
-        WelcomeUserMailer.welcome_email_invited(@invitation,json_body['password'],ENV['DOMAIN']).deliver
-      puts "Email sent successfully"
+    puts "Calling ad replicate.."
+    begin
+      ad_sync_url = "#{ENV['API_HOST']}/ad/replicate"
+      response = RestClient.post(url=ad_sync_url,payload={:uname => 'demo.user'}, headers= {:token => ENV["API_KEY"]})
+      puts response.body
+    rescue => e
+      puts e
     end
+    puts "AD replicate called successfully"
 
     puts "Creating user profile directory in home region.."
     begin
@@ -87,15 +84,18 @@ class SignupWorker
     end
     puts "Sync success"
 
-    puts "Calling ad replicate.."
-    begin
-      ad_sync_url = "#{ENV['API_HOST']}/ad/replicate"
-      response = RestClient.post(url=ad_sync_url,payload={:uname => 'demo.user'}, headers= {:token => ENV["API_KEY"]})
-      puts response.body
-    rescue => e
-      puts e
+    # Find a way to optimize this
+    domain_name = @invitation.recipient_email.split("@").last
+    @domain = Domain.find_by_name(domain_name)
+    if !@domain.nil? && @domain.status == 'active'
+      puts "Sending Super User email...."
+        WelcomeUserMailer.welcome_email(@invitation,json_body['password'],ENV['DOMAIN']).deliver
+      puts "Email sent successfully"
+    else
+      puts "Sending Regular User email...."
+        WelcomeUserMailer.welcome_email_invited(@invitation,json_body['password'],ENV['DOMAIN']).deliver
+      puts "Email sent successfully"
     end
-    puts "AD replicate called successfully"
 
   end
 end
