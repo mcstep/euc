55794c3b9b29e4a4a718f2ab96ffbefeb1abc48e
Ashutosh Joshi <ashutoshj@ubuntu.(none)>
2014-06-19 18:28:14 -0700

    disabled replication for now

--- app/workers/signup_worker.rb
+++ app/workers/signup_worker.rb
@@ -28,15 +28,16 @@ class SignupWorker
 
     puts "Done creating account. Response from AD #{json_body}"
    
-    puts "Calling ad replicate.."
-    begin
-      ad_sync_url = "#{ENV['API_HOST']}/ad/replicate"
-      response = RestClient.post(url=ad_sync_url,payload={:uname => 'demo.user'}, headers= {:token => ENV["API_KEY"]})
-      puts response.body
-    rescue => e
-      puts e
-    end
-    puts "AD replicate called successfully"
+    #puts "Calling ad replicate.."
+    #begin
+    #  ad_sync_url = "#{ENV['API_HOST']}/ad/replicate"
+    #  #response = RestClient.post(url=ad_sync_url,payload={:uname => 'demo.user'}, headers= {:token => ENV["API_KEY"]})
+    #  RestClient::Request.execute(:method => :post, :url => ad_sync_url, :timeout => 200, :open_timeout => 10,  :payload => {:uname => 'demo.user'},  :headers => {:token => ENV["API_KEY"]})
+    #  puts response.body
+    #rescue => e
+    #  puts e
+    #end
+    #puts "AD replicate called successfully"
 
     puts "Creating user profile directory in home region.."
     begin
