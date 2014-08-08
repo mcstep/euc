8121bd441475dad7d0dc415a6fc1ea03213415bf
Ashutosh Joshi <ashutoshj@ubuntu.(none)>
2014-08-08 13:53:02 -0700

    add recipient email address for better logging in email workers

--- app/workers/account_expire_email_worker.rb
+++ app/workers/account_expire_email_worker.rb
@@ -3,7 +3,7 @@ class AccountExpireEmailWorker
 
   def perform(invitation_id)
     @invitation = Invitation.with_deleted.find_by_id(invitation_id)	 
-    puts "Sending account expiry email to recipient:"
+    puts "Sending account expiry email to recipient: #{@invitation.recipient_email}"
     WelcomeUserMailer.account_expiry_email(@invitation).deliver
     puts "Account expiry email sent successfully"
   end
--- app/workers/account_extension_email_worker.rb
+++ app/workers/account_extension_email_worker.rb
@@ -4,7 +4,7 @@ class AccountExtensionEmailWorker
   def perform(invitation_id, extension_id)
     @invitation = Invitation.find_by_id(invitation_id)	 
     @extension = Extension.find_by_id(extension_id)	 
-    puts "Sending account extension email to recipient:"
+    puts "Sending account extension email to recipient: #{@invitation.recipient_email}"
     WelcomeUserMailer.account_extension_email(@invitation, @extension).deliver
     puts "Account extension email sent successfully"
   end
