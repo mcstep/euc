524c4689b828710397c74a2e8b3269ef1c1dfe28
Ashutosh Joshi <ashutoshj@ubuntu.(none)>
2015-03-11 21:24:29 -0700

    remove typo from ad recplicate log

--- app/workers/account_active_directory_america_replicate_worker.rb
+++ app/workers/account_active_directory_america_replicate_worker.rb
@@ -8,7 +8,7 @@ class AccountActiveDirectoryAmericaReplicateWorker
        ad_sync_url = "#{ENV['API_HOST']}/ad/replicate/amer"
        #response = RestClient.post(url=ad_sync_url,payload={:uname => 'demo.user'}, headers= {:token => ENV["API_KEY"]})
        RestClient::Request.execute(:method => :post, :url => ad_sync_url, :timeout => 200, :open_timeout => 10,  :payload => {:uname => 'demo.user'},  :headers => {:token => ENV["API_KEY"]})
-       puts response.body
+       #puts response.body
       rescue => e
        puts e
       end
