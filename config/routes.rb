3cc0ab9c3334965086a364a7c018d837b16f5e93
Leonard Hecker <leonard@hecker.io>
2015-04-10 17:39:22 +0200

    cleaned up routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,58 +1,45 @@
 Rails.application.routes.draw do
-  resources :reg_codes
+  root 'home#index'
 
-  get 'password_change/check_password'
-
-  get 'password_change/change_password'
-
+  get 'dashboard' => 'home#index', :as => 'dashboard'
+  get 'forgotpassword' => 'sessions#new', :as => 'forgotpassword'
+  get 'invitation_search' => 'invitations#search', :as => 'invitation_search'
+  get 'log_in' => 'sessions#new', :as => 'log_in'
+  get 'log_out' => 'sessions#destroy', :as => 'log_out'
+  get 'register' => 'signup#new', :as => 'register'
+  get 'register/:reg_code', to: 'signup#reg_code', :as => 'registration_code_signup'
+  get 'toggle' => 'domains#toggle', :as => 'toggle'
+  get 'toggle_airwatch' => 'eula#toggle_airwatch', :as => 'toggle_airwatch'
   get 'eula/accept'
-
-  get 'reports/stats' => "reports#potential_seats", :as => "reports_seats_stats"
-
-  get 'support_request/create'
-
+  get 'home/index'
+  get 'password_change/change_password'
+  get 'password_change/check_password'
   get 'password_reset/create'
-
+  get 'reports/stats' => 'reports#potential_seats', :as => 'reports_seats_stats'
+  get 'sessions/new'
   get 'signup/new'
+  get 'support_request/create'
+  post 'change_password' => 'password_change#change_password', :as => 'change_password'
+  post 'eula' => 'eula#create', :as => 'eula'
+  post 'extend_invitation' => 'invitations#extend', :as => 'extend_invitation'
+  post 'impersonate_user' => 'invitations#impersonate', :as => 'impersonate_user'
+  post 'password_reset' => 'password_reset#create', :as => 'password_reset'
+  post 'support_request' => 'support_request#create', :as => 'support_request'
+  post 'unimpersonate' => 'invitations#unimpersonate', :as => 'unimpersonate'
 
-  #resources :invitations
+  resources :domains
+  resources :reg_codes
+  resources :sessions
+  resources :signup
+
   resources :invitations do
     collection do
       get 'check_invitation'
     end
   end
 
-  root 'home#index'
-
-  get "log_out" => "sessions#destroy", :as => "log_out"
-  get "log_in" => "sessions#new", :as => "log_in"
-  get "forgotpassword" => "sessions#new", :as => "forgotpassword"
-  get "register" => "signup#new", :as => "register"
-  get "register/:reg_code", to: 'signup#reg_code', :as => "registration_code_signup"
-  get "dashboard" => "home#index", :as => "dashboard"
-  post "password_reset" => "password_reset#create", :as => "password_reset"
-  post "support_request" => "support_request#create", :as => "support_request"
-  post "extend_invitation" => "invitations#extend", :as => "extend_invitation"
-  post "impersonate_user" => "invitations#impersonate", :as => "impersonate_user"
-  post "unimpersonate" => "invitations#unimpersonate", :as => "unimpersonate"
-  post "eula" => "eula#create", :as => "eula"
-  get "invitation_search" => "invitations#search", :as => "invitation_search"
-
-  post "change_password" => "password_change#change_password", :as => "change_password"
-  
-  get "toggle" => "domains#toggle", :as => "toggle"
-  get "toggle_airwatch" => "eula#toggle_airwatch", :as => "toggle_airwatch"
-
-  get 'home/index'
-
-  get 'sessions/new'
-
-  resources :domains
-  resources :sessions
-  resources :signup
-
   require 'sidekiq/web'
-  
+
   class SessionAuthenticatedConstraint
     def self.matches?(request)
       !request.session[:user_id].blank?
@@ -80,6 +67,4 @@ Rails.application.routes.draw do
       end
     end
   end
-
-
 end
