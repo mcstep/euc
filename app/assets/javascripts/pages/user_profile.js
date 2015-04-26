18e638c73f0670f96dad2dd517ff97cd8666321e
Leonard Hecker <leonard@hecker.io>
2015-04-26 18:42:46 +0200

    fixes related to the webcam feature

--- app/assets/javascripts/pages/user_profile.js
+++ app/assets/javascripts/pages/user_profile.js
@@ -1,147 +1,154 @@
 (function () {
 	function page_scripts() {
-        var $editProfileForm = $('#edit-profile-form');
+		var $editProfileForm = $('#edit-profile-form');
 
-        if ($editProfileForm.length) {
-            function setRemoveAvatar(remove) {
-                document.getElementById('edit-profile-remove-avatar').checked = remove;
-            }
+		if ($editProfileForm.length) {
+			function setRemoveAvatar(remove) {
+				document.getElementById('edit-profile-remove-avatar').checked = remove;
+			}
 
-            // we manually append to file input to assert that it's value is unset
-            var $avatarFile = $('<input class="hidden" type="file"/>').appendTo($editProfileForm);
+			// we manually append to file input to assert that it's value is unset
+			var $avatarFile = $('<input class="hidden" type="file"/>').appendTo($editProfileForm);
 
-            var $editProfileModal = $('#edit-profile-modal');
-            var $generalAvatarButtons = $('#edit-profile-left-buttons').children();
-            var $generalTakePhoto = $generalAvatarButtons.eq(0);
-            var $generalUploadPhoto = $generalAvatarButtons.eq(1);
-            var $generalRemovePhoto = $generalAvatarButtons.eq(2);
+			var $editProfileModal = $('#edit-profile-modal');
+			var $generalAvatarButtons = $('#edit-profile-left-buttons').children();
+			var $generalTakePhoto = $generalAvatarButtons.eq(0);
+			var $generalUploadPhoto = $generalAvatarButtons.eq(1);
+			var $generalRemovePhoto = $generalAvatarButtons.eq(2);
 
-            setRemoveAvatar(false);
+			setRemoveAvatar(false);
 
-            $avatarFile.on('change', function () {
-                if (this.value) {
-                    setRemoveAvatar(false);
-                    $avatarFile.attr('name', 'user[avatar]');
-                    $editProfileForm.submit();
-                }
-            });
+			$avatarFile.on('change', function () {
+				if (this.value) {
+					setRemoveAvatar(false);
+					$avatarFile.attr('name', 'user[avatar]');
+					$editProfileForm.submit();
+				}
+			});
 
-            $generalUploadPhoto.on('click', function () {
-                $avatarFile.click();
-            });
+			$generalUploadPhoto.on('click', function () {
+				$avatarFile.click();
+			});
 
-            $generalRemovePhoto.on('click', function () {
-                setRemoveAvatar(true);
-                $avatarFile.removeAttr('name');
-                $editProfileForm.submit();
-            });
+			$generalRemovePhoto.on('click', function () {
+				setRemoveAvatar(true);
+				$avatarFile.removeAttr('name');
+				$editProfileForm.submit();
+			});
 
-            $editProfileForm.on('submit', function () {
-                $editProfileModal.modal('hide');
-            });
+			$editProfileForm.on('submit', function () {
+				$editProfileModal.modal('hide');
+			});
 
+			$editProfileModal.on('show.bs.modal', function () {
+				$editProfileModal.removeClass('webcam-active');
+			});
 
-            var editProfileWebcamCanvas = document.createElement('canvas');
+			var editProfileWebcamCanvas = document.createElement('canvas');
 
-            if (typeof ImageData !== 'function' || !editProfileWebcamCanvas || !editProfileWebcamCanvas.toDataURL) {
-                $generalTakePhoto.prop('disabled', true);
-            } else {
-                function toggleEditProfileMode() {
-                    $editProfileModal.toggleClass('webcam-active');
-                }
+			if (typeof ImageData !== 'function' || !editProfileWebcamCanvas || !editProfileWebcamCanvas.toDataURL) {
+				$generalTakePhoto.prop('disabled', true);
+			} else {
+				function toggleEditProfileMode() {
+					$editProfileModal.toggleClass('webcam-active');
+				}
 
-                function setWebcamShotDisabled(disabled) {
-                    $webcamShot.prop('disabled', disabled);
-                }
+				function setWebcamShotDisabled(disabled) {
+					$webcamShot.prop('disabled', disabled);
+				}
 
-                var $editProfileWebcam = $('#edit-profile-webcam');
-                var $webcamShot = $('#edit-profile-webcam-shot');
-                var $editProfileWebcamContainer = $('div', $editProfileWebcam);
-                var $editProfileWebcamSave = $('.btn-success', $editProfileWebcam);
-                var $editProfileWebcamCancel = $('.btn-default', $editProfileWebcam);
-                var $avatarDataField = null;
-                var width = $editProfileWebcamContainer.width();
-                var height = $editProfileWebcamContainer.height();
-                var ctx = editProfileWebcamCanvas.getContext('2d');
-                var image = ctx.getImageData(0, 0, width, height);
-                var initialized = false;
+				var $editProfileWebcam = $('#edit-profile-webcam');
+				var $webcamShot = $('#edit-profile-webcam-shot');
+				var $editProfileWebcamContainer = $('div', $editProfileWebcam);
+				var $editProfileWebcamSave = $('.btn-success', $editProfileWebcam);
+				var $editProfileWebcamCancel = $('.btn-default', $editProfileWebcam);
+				var $avatarDataField = null;
+				var width = $editProfileWebcamContainer.width();
+				var height = $editProfileWebcamContainer.height();
+				var ctx = editProfileWebcamCanvas.getContext('2d');
+				var image = ctx.getImageData(0, 0, width, height);
+				var initialized = false;
 
-                editProfileWebcamCanvas.setAttribute('width', width);
-                editProfileWebcamCanvas.setAttribute('height', height);
+				editProfileWebcamCanvas.setAttribute('width', width);
+				editProfileWebcamCanvas.setAttribute('height', height);
 
-                $editProfileWebcamCancel.on('click', function () {
-                    toggleEditProfileMode();
-                });
+				$editProfileWebcamCancel.on('click', function () {
+					toggleEditProfileMode();
+				});
 
-                $generalTakePhoto.on('click', function () {
-                    toggleEditProfileMode();
+				$generalTakePhoto.on('click', function () {
+					toggleEditProfileMode();
 
-                    if (!initialized) {
-                        initialized = true;
+					if (!initialized) {
+						initialized = true;
 
-                        $editProfileWebcamContainer.webcam({
-                            swffile: '/assets/sAS3Cam.swf',
+						$editProfileWebcamContainer.webcam({
+							swffile: '/assets/sAS3Cam.swf',
 
-                            previewWidth: width,
-                            previewHeight: height,
+							previewWidth: width,
+							previewHeight: height,
 
-                            resolutionWidth: width,
-                            resolutionHeight: height,
+							resolutionWidth: width,
+							resolutionHeight: height,
 
-                            StageScaleMode: 'noBorder',
+							StageScaleMode: 'noBorder',
 
-                            mode: 'callback',
+							mode: 'callback',
 
-                            debug: function (type, string) {
-                                console.log(type + ": " + string);
-                            },
+							debug: function (type, string) {
+								console.log(type + ": " + string);
 
-                            noCameraFound: function () {
-                                this.debug('error', 'Web camera is not available');
-                            },
+								if (type === 'error') {
+									$editProfileWebcamContainer.html('<div>' + string + '</div>');
+								}
+							},
 
-                            swfApiFail: function (e) {
-                                this.debug('error', 'Internal camera plugin error');
-                            },
+							noCameraFound: function () {
+								this.debug('error', 'Camera is not available.');
+							},
 
-                            cameraDisabled: function () {
-                                this.debug('error', 'Please allow access to your camera');
-                                $webcamShot.prop('disabled', true);
-                            },
+							swfApiFail: function (e) {
+								this.debug('error', 'Internal camera plugin error.');
+							},
 
-                            cameraEnabled:  function () {
-                                var self = this;
+							cameraDisabled: function () {
+								this.debug('error', 'Please allow access to your camera.');
+								$webcamShot.prop('disabled', true);
+							},
 
-                                if (self.isCameraEnabled) {
-                                    return;
-                                }
+							cameraEnabled:  function () {
+								var self = this;
 
-                                self.isCameraEnabled = true;
-                                self.debug('notice', 'Camera enabled');
-                                self.setCamera('0');
+								if (self.isCameraEnabled) {
+									return;
+								}
 
-                                $webcamShot.prop('disabled', false).on('click', function () {
-                                    var result = self.save();
+								self.isCameraEnabled = true;
+								self.debug('notice', 'Camera enabled');
+								self.setCamera('0');
 
-                                    if (result) {
-                                        if (!$avatarDataField) {
-                                            $avatarDataField = $('<textarea class="hidden" name="user[avatar_data_uri]"></textarea>').appendTo($editProfileForm);
-                                        }
+								$webcamShot.prop('disabled', false).on('click', function () {
+									var result = self.save();
 
-                                        $avatarDataField.val('data:image/jpeg;base64,' + result);
-                                        $editProfileWebcamSave.prop('disabled', false);
+									if (result) {
+										if (!$avatarDataField) {
+											$avatarDataField = $('<textarea class="hidden" name="user[avatar_data_uri]"></textarea>').appendTo($editProfileForm);
+										}
 
-                                        self.setCamera('0');
-                                    } else {
-                                        self.debug('error', 'Broken camera');
-                                    }
-                                });
-                            }
-                        });
-                    }
-                });
-            }
-        }
+										$avatarDataField.val('data:image/jpeg;base64,' + result);
+										$editProfileWebcamSave.prop('disabled', false);
+
+										self.setCamera('0');
+									} else {
+										self.debug('error', 'Broken camera');
+									}
+								});
+							}
+						});
+					}
+				});
+			}
+		}
 	}
 
 	$(page_scripts);
