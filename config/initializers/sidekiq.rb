22c12180d19954af7b37a210ba3f6c1c8e58b163
Ashutosh Joshi <ashutoshj@ubuntu.(none)>
2015-06-02 17:06:19 -0700

    fix redis ENV variables

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,6 @@
 Sidekiq.configure_server do |config|
   if Rails.env == 'development'
     config.redis = { :url => ENV["REDIS_URL"], :namespace => ENV["REDIS_NAMESPACE"] }
-  else
-    config.redis = { :url => 'redis://pub-redis-14604.us-east-1-1.1.ec2.garantiadata.com:14604', :namespace => 'portal' }
   end
 end
 
@@ -10,15 +8,5 @@ end
 Sidekiq.configure_client do |config|
   if Rails.env == 'development'
     config.redis = { :url => ENV["REDIS_URL"], :namespace => ENV["REDIS_NAMESPACE"] }
-  else
-    config.redis = { :url => 'redis://pub-redis-14604.us-east-1-1.1.ec2.garantiadata.com:14604', :namespace => 'portal' }
   end
 end
-
-#Sidekiq.configure_server do |config|
-#  config.redis = { :url => 'redis://pub-redis-11927.us-east-1-3.3.ec2.garantiadata.com:11927', :namespace => 'portal' }
-#end
-
-#Sidekiq.configure_client do |config|
-#  config.redis = { :url => 'redis://pub-redis-11927.us-east-1-3.3.ec2.garantiadata.com:11927', :namespace => 'portal' }
-#end
