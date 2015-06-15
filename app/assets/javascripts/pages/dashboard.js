46141148cdbf6b761c6498bcffbad7e1361a3f1e
Leonard Hecker <leonard@hecker.io>
2015-06-15 17:11:47 +0200

    open launcher tiles on mouseenter

--- app/assets/javascripts/pages/dashboard.js
+++ app/assets/javascripts/pages/dashboard.js
@@ -3,4 +3,13 @@
 	$('#eula-modal').one('shown.bs.modal', function () {
 		$('.modal-body', this).append('<iframe src="https://docs.google.com/gview?url=http://www.air-watch.com/downloads/legal/20140701_AirWatch_EULA.pdf&amp;embedded=true" width="100%" height="0"><p>Your browser does not support iframes.</p></iframe>');
 	});
+
+	$('#dashboard-launcher').children().on('mouseenter mouseleave', function (e) {
+		var $this = $(this);
+		var shouldBeOpen = e.type === 'mouseenter';
+
+		if ($this.hasClass('open') !== shouldBeOpen) {
+			$this.find('[data-toggle="dropdown"]').dropdown('toggle');
+		}
+	});
 })();
