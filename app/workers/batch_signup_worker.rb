db476040afa49d2d4dc6a83cc91e572c096faa7c
Ashutosh Joshi <ashutoshj@ubuntu.(none)>
2015-07-07 16:00:07 -0700

    custom worker and rake task for batch user creation

---
+++ app/workers/batch_signup_worker.rb
@@ -0,0 +1,88 @@
+class BatchSignupWorker
+  # Same as SignupWorker but without the AD Replicate call that takes forever - TEMPORARY until we move to new schema/app
+  include Sidekiq::Worker
+
+  def perform(invitation_id)
+    @invitation = Invitation.find_by_id(invitation_id)
+    puts "Will start working on Invitation ID#{invitation_id} for Recipient #{@invitation.recipient_email}"
+
+    region = "dldc"
+    if !@invitation.region.nil?
+      region = @invitation.region.downcase
+    end
+
+    account_created = false
+    begin
+      response = RestClient.post(url="#{ENV['API_HOST']}/signup", payload ={:fname => @invitation.recipient_firstname, :lname => @invitation.recipient_lastname, :username => @invitation.recipient_username, :org => @invitation.recipient_company, :email => @invitation.recipient_email, :title => @invitation.recipient_title, :expires_at => ((@invitation.expires_at.to_i)*1000), :region => region}, headers={:token => ENV["API_KEY"]})
+
+      if response.code == 200
+        json_body = JSON.parse response
+        @invitation.recipient_username = json_body['username']
+        @invitation.save!
+        account_created = true
+      end
+    rescue => e
+      #@invitation.errors[:base] << "Sorry! Could not contact the AD Server at this time. Please try again later!"
+      logger.error "error contacting AD server"
+      puts e
+    end
+
+    puts "Done creating account. Response from AD #{json_body}"
+
+    if account_created == true
+      # TODO: merge this and the following block?
+      puts "Creating user profile directory in home region.."
+      begin
+        create_dir_url = "#{ENV['API_HOST']}/createdir"
+        response = RestClient.post(url=create_dir_url,payload={:username => @invitation.recipient_username, :region => region}, headers= {:token => ENV["API_KEY"]})
+        puts response.body
+      rescue => e
+        puts e
+      end
+      puts "Created user profile directory successfully in home region"
+
+      puts "Creating user profile directory in remaining regions.."
+      rem_regions = ['amer', 'dldc', 'emea', 'apac'] - ["#{region}"]
+      rem_regions.each do |sync_reg|
+        begin
+          create_dir_url = "#{ENV['API_HOST']}/createdir"
+          response = RestClient.post(url=create_dir_url,payload={:username => @invitation.recipient_username, :region => sync_reg}, headers= {:token => ENV["API_KEY"]})
+          puts response.body
+        rescue => e
+          puts e
+        end
+      end
+      puts "Created user profile directory successfully in other regions"
+
+      # Find a way to optimize this
+      super_user = false
+      # find the corresponding invitation and check if the user signed up with a reg_code
+      inv_for_user = @invitation
+      if !inv_for_user.nil? && !inv_for_user.reg_code_id.nil?
+        reg_code = RegCode.find_by_id(inv_for_user.reg_code_id)
+        if !reg_code.nil? && (reg_code.account_type == 1) # 1 = vip
+          super_user = true
+        end
+      else
+        domain_name = @invitation.recipient_email.split("@").last
+        @domain = Domain.find_by_name(domain_name)
+        if !@domain.nil? && @domain.status == 'active'
+          super_user = true
+        end
+      end
+
+      if super_user == true
+        puts "Sending Super User email...."
+          WelcomeUserMailer.welcome_email(@invitation,json_body['password'],ENV['DOMAIN']).deliver
+        puts "Email sent successfully"
+      else
+        puts "Sending Regular User email...."
+          WelcomeUserMailer.welcome_email_invited(@invitation,json_body['password'],ENV['DOMAIN']).deliver
+        puts "Email sent successfully"
+      end
+
+    else
+      puts "AD account creation failed for #{@invitation.recipient_email}. Will stop provisioning"
+    end
+  end
+end
---
+++ lib/tasks/batch_user_create.rake
@@ -0,0 +1,54 @@
+desc "This class is used to batch create user accounts on VMware Testdrive Portal"
+
+  task :batch_user_create, [:arg0] => :environment do |t, args|
+    require "csv"
+    require 'net/http'
+    require 'rest-client'
+
+    puts "BATCH: Args were: #{args}"
+    uri = URI(args[:arg0])
+    csv_text = Net::HTTP.get(uri)
+    csv = CSV.parse(csv_text, :headers=>true)
+    csv.each do |row|
+      firstname = row[0].downcase
+      lastname = row[1].downcase
+      email = row[2].downcase
+      job_title = row[3]
+      company = row[4]
+      region = row[5]
+
+      @invitation = Invitation.new
+      @invitation.recipient_firstname = firstname
+      @invitation.recipient_lastname = lastname
+      @invitation.recipient_email = email
+      @invitation.region = region
+      @invitation.recipient_company = company
+      @invitation.recipient_title = job_title
+
+      begin
+        if @invitation.save!
+          puts "BATCH: Invitation successfully created for #{email}. Will call AD provisioning for Invitation ID #{@invitation.id}"
+          BatchSignupWorker.new.perform(@invitation.id)
+        end
+      rescue Exception => e
+        puts "BATCH: Error creating Invitation for #{email}. Exception: #{e}"
+        next
+      end
+    end
+
+    puts "BATCH: Done with creation of accounts. Will call Workspace Portal sync.."
+    begin
+      home_sync_url = "#{ENV['API_HOST']}/sync/dldc"
+      response = RestClient.post(url=home_sync_url,
+                                 payload={:uname => 'demo.user'}, 
+                                 headers= {:token => ENV["API_KEY"]})
+      puts response.body
+    rescue => e
+      puts e
+    end
+    puts "BATCH: Sync success for Workspace Portal"
+
+
+    puts "BATCH: Done with Workspace Portal Sync. Will call AD Replicate.."
+    AccountActiveDirectoryReplicateWorker.perform_async
+  end
