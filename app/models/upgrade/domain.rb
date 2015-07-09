2e4b62fc0184979e67aa7b42b09b4724e524083f
Boris Staal <boris@staal.io>
2015-07-09 23:50:21 +0300

    Config switched back to SQLite

--- app/models/upgrade/domain.rb
+++ app/models/upgrade/domain.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: domains
+#
+#  id         :integer          not null, primary key
+#  company_id :integer
+#  profile_id :integer
+#  name       :string
+#  status     :integer          default(0), not null
+#  limit      :integer
+#  deleted_at :datetime
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_domains_on_company_id  (company_id)
+#  index_domains_on_deleted_at  (deleted_at)
+#  index_domains_on_profile_id  (profile_id)
+#
+
 class Upgrade::Domain < ActiveRecord::Base
   self.table_name = "domains"
 
