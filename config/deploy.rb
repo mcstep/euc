fa05c00f7ad9ca4eae2ec921869be68d301c19ad
Boris Staal <boris@staal.io>
2015-07-14 18:13:33 +0300

    Seed capistrano task

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,3 +8,16 @@ set :scm, :git
 set :branch, 'rework'
 set :keep_releases, 5
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+namespace :deploy do
+  desc 'Runs rake db:seed'
+  task :seed => [:set_rails_env] do
+    on primary fetch(:migration_role) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, "db:seed"
+        end
+      end
+    end
+  end
+end
