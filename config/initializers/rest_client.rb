84ca209d551a8a10c64c24d2c2e6bafac23a1085
Boris Staal <boris@staal.io>
2015-08-06 16:41:02 +0300

    RestClient patch improved to not break block

--- config/initializers/rest_client.rb
+++ config/initializers/rest_client.rb
@@ -4,9 +4,16 @@ module RestClient
       Rails.logger.info("RestClient - #{method.try(:upcase)} #{url.inspect} #{headers.inspect if headers.present?} #{payload.inspect if payload.present?}")
 
       begin
-        result = execute_without_logging
-        Rails.logger.info "RestClient - #{result.code}\n#{result.body}"
-        return result
+        if block_given?
+          execute_without_logging do |result, *args|
+            Rails.logger.info "RestClient - #{result.code}\n#{result.body}"
+            yield result, *args
+          end
+        else
+          result = execute_without_logging
+          Rails.logger.info "RestClient - #{result.code}\n#{result.body}"
+          return result
+        end
       rescue RestClient::Exception => e
         Rails.logger.warn "RestClient - #{e.response.code}\n#{e.response.body}"
         raise e
