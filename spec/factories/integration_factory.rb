730b7bbbe533402c6b389145a535c3167398430e
Boris Staal <boris@staal.io>
2015-08-10 19:42:33 +0300

    Getting rid of build strategy for FG

--- spec/factories/integration_factory.rb
+++ spec/factories/integration_factory.rb
@@ -2,32 +2,32 @@ FactoryGirl.define do
   factory :integration do
     name        { FFaker::Lorem.words(2).join(' ') }
     domain      { FFaker::Internet.domain_name }
-    association :directory, factory: :directory, strategy: :build
+    association :directory, factory: :directory
 
     factory :directory_integration do
-      association :directory, factory: :test_directory, strategy: :build
+      association :directory, factory: :test_directory
     end
 
     factory :airwatch_integration do
-      association :airwatch_instance, factory: :airwatch_instance, strategy: :build
+      association :airwatch_instance, factory: :airwatch_instance
     end
 
     factory :google_apps_integration do
-      association :google_apps_instance, factory: :google_apps_instance, strategy: :build
+      association :google_apps_instance, factory: :google_apps_instance
     end
 
     factory :horizon_view_integration do
-      association :horizon_view_instance, factory: :horizon_instance, strategy: :build
+      association :horizon_view_instance, factory: :horizon_instance
     end
 
     factory :office365_integration do
-      association :office365_instance, factory: :office365_instance, strategy: :build
+      association :office365_instance, factory: :office365_instance
     end
 
     factory :complete_integration do
-      association :airwatch_instance, factory: :airwatch_instance, strategy: :build
-      association :google_apps_instance, factory: :google_apps_instance, strategy: :build
-      association :horizon_view_instance, factory: :horizon_instance, strategy: :build
+      association :airwatch_instance, factory: :airwatch_instance
+      association :google_apps_instance, factory: :google_apps_instance
+      association :horizon_view_instance, factory: :horizon_instance
     end
   end
 end
--- spec/factories/user_integration_factory.rb
+++ spec/factories/user_integration_factory.rb
@@ -3,19 +3,19 @@ FactoryGirl.define do
     username                  { FFaker::Internet.user_name }
     directory_expiration_date { Date.tomorrow }
 
-    association :user, factory: :user, strategy: :build
-    association :integration, factory: :integration, strategy: :build
+    association :user, factory: :user
+    association :integration, factory: :integration
 
     factory :airwatch_user_integration do
-      association :integration, factory: :airwatch_integration, strategy: :build
+      association :integration, factory: :airwatch_integration
     end
 
     factory :google_apps_user_integration do
-      association :integration, factory: :google_apps_integration, strategy: :build
+      association :integration, factory: :google_apps_integration
     end
 
     factory :complete_user_integration do
-      association :integration, factory: :complete_integration, strategy: :build
+      association :integration, factory: :complete_integration
     end
   end
 end
