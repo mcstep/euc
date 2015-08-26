bc9e5cc1a24221d1ac43888f54c809468ef13f1a
Boris Staal <boris@staal.io>
2015-08-27 01:01:05 +0300

    Airwatch form extended

--- app/policies/airwatch_instance_policy.rb
+++ app/policies/airwatch_instance_policy.rb
@@ -1,6 +1,10 @@
 class AirwatchInstancePolicy < ApplicationPolicy
   def permitted_attributes
-    [:display_name, :group_name, :group_region, :api_key, :host, :user, :password, :parent_group_id, :admin_roles_text]
+    [
+      :display_name, :group_name, :group_region, :api_key, :host, :user, :password,
+      :parent_group_id, :admin_roles_text, :use_groups, :use_admin, :use_templates,
+      :templates_api_url, :templates_token
+    ]
   end
 
   def index?
