4edd8c7520fd4066c5cee4a88a93c58f6bbdfd21
Ashutosh Joshi <ashutoshj@ubuntu.(none)>
2015-08-27 19:52:36 -0700

    add security_pin to aw_instance permitted attributes

--- app/policies/airwatch_instance_policy.rb
+++ app/policies/airwatch_instance_policy.rb
@@ -2,8 +2,8 @@ class AirwatchInstancePolicy < ApplicationPolicy
   def permitted_attributes
     [
       :display_name, :group_name, :group_region, :api_key, :host, :user, :password,
-      :parent_group_id, :admin_roles_text, :use_groups, :use_admin, :use_templates,
-      :templates_api_url, :templates_token
+      :security_pin, :parent_group_id, :admin_roles_text, :use_groups, :use_admin,
+      :use_templates, :templates_api_url, :templates_token
     ]
   end
 
