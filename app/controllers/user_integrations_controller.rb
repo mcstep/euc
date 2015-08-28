aeda9c666b1c9a2c2cf4b62707f63933401a4185
Boris Staal <boris@staal.io>
2015-08-29 01:41:36 +0300

    Proper modals for services toggler

--- app/controllers/user_integrations_controller.rb
+++ app/controllers/user_integrations_controller.rb
@@ -10,11 +10,9 @@ class UserIntegrationsController < ApplicationController
       user_integration.save!
 
       if state_machine.disabled?
-        flash[:service] = I18n.t('flash.service_disabled', service: params[:service].camelize)
-        redirect_to services_current_user_path
+        redirect_to services_current_user_path, notice: I18n.t('flash.service_disabled', service: params[:service].camelize)
       else
-        flash[:service] = I18n.t('flash.service_enabled', service: params[:service].camelize)
-        redirect_to services_current_user_path
+        redirect_to services_current_user_path, notice: I18n.t('flash.service_enabled', service: params[:service].camelize)
       end
     else
       redirect_to services_current_user_path, error: I18n.t('flash.service_not_toggled')
