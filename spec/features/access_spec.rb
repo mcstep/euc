d548d3ee1079943b85b9b66366debe867e040cae
Boris Staal <boris@staal.io>
2015-09-09 18:01:29 +0300

    Non-roots allowed to see reporting

--- spec/features/access_spec.rb
+++ spec/features/access_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Modals', :js, type: :feature do
+RSpec.describe 'Access', type: :feature do
   subject{ page }
   before{ create_cookie 'user_id', user.id }
 
@@ -17,6 +17,20 @@ RSpec.describe 'Modals', :js, type: :feature do
       let(:user){ create(:admin, total_invitations: 0) }
       it{ is_expected.to_not have_link('Invite user') }
       it{ is_expected.to_not have_link('Send Invitation') }
+    end
+  end
+
+  describe 'Reporting visibility' do
+    before{ visit '/' }
+
+    context 'when user is root with can_see_reports = false' do
+      let(:user){ create(:root, can_see_reports: false) }
+      it{ is_expected.to_not have_link('Reporting') }
+    end
+
+    context 'when user is basic with can_see_reports = true' do
+      let(:user){ create(:user, can_see_reports: true) }
+      it{ is_expected.to have_link('Reporting') }
     end
   end
 end
