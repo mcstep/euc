7756653590a67037d03320e49b1aaab85a483f02
Ashutosh Joshi <ashutoshj@ubuntu.(none)>
2015-09-22 13:35:50 -0700

    change id fields for sfdc mass api call

--- app/models/salesforce_instance.rb
+++ app/models/salesforce_instance.rb
@@ -89,12 +89,12 @@ class SalesforceInstance < ActiveRecord::Base
 
   def find_changed_dealregs
     client.get('/services/apexrest/v1.0/EucDemoRestService/EUC', objType: 'ORTN', recId: 'All')
-      .body.instance_variable_get('@raw_page')['records'].to_a.map{|x| x['RecId']}
+      .body.instance_variable_get('@raw_page')['records'].to_a.map{|x| x['ORTN']}
   end
 
   def find_changed_opportunities
     client.get('/services/apexrest/v1.0/EucDemoRestService/EUC', objType: 'Deal', recId: 'All')
-      .body.instance_variable_get('@raw_page')['records'].to_a.map{|x| x['RecId']}
+      .body.instance_variable_get('@raw_page')['records'].to_a.map{|x| x['Opportunity Id']}
   end
 
   def update(id, settings)
