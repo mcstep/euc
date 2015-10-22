afb21c5b2516593a040eee0f04bb74e1a9149dce
Boris Staal <boris@staal.io>
2015-10-22 15:16:12 +0300

    Exclude vmware from stats for regular users

--- app/controllers/reporting_controller.rb
+++ app/controllers/reporting_controller.rb
@@ -5,24 +5,35 @@ class ReportingController < ApplicationController
     params[:companies] ||= []
     params[:regions]   ||= []
 
+    exclusion = '%vmware%'
+
     if params[:start_date].present?
-      @users = User.includes(sent_invitations: :to_user).where('created_at > ?', Date.parse(params[:start_date]))
-      @invitations = Invitation.where('created_at > ?', Date.parse(params[:start_date]))
+      @users = User.includes(sent_invitations: :to_user).where('users.created_at > ?', Date.parse(params[:start_date]))
+      @invitations = Invitation.where('invitations.created_at > ?', Date.parse(params[:start_date]))
 
       if params[:end_date].present?
-        @users = @users.where('created_at < ?', Date.parse(params[:end_date]))
-        @invitations = @invitations.where('created_at < ?', Date.parse(params[:end_date]))
+        @users = @users.where('users.created_at < ?', Date.parse(params[:end_date]))
+        @invitations = @invitations.where('invitations.created_at < ?', Date.parse(params[:end_date]))
       end
 
       @users = @users.where(company_id: params[:companies]) if params[:companies].present?
       @users = @users.where(home_region: params[:regions]) if params[:regions].present?
+
+      unless current_user.root?
+        @users = @users.joins(:company).where("companies.name NOT ILIKE ?", exclusion)
+        @invitations = @invitations.joins(to_user: :company).where("companies.name NOT ILIKE ?", exclusion)
+      end
     end
 
     @total = User.with_deleted
     @total = @total.where(company_id: params[:companies]) if params[:companies].present?
     @total = @total.where(home_region: params[:regions]) if params[:regions].present?
 
-    @opportunity = Invitation.joins(:to_user).merge(@total).pluck(:potential_seats).compact.inject(:+)
+    @opportunity = Invitation.joins(:to_user).merge(@total)
+    @opportunity = @opportunity.joins(to_user: :company).where("companies.name NOT ILIKE ?", exclusion) unless current_user.root?
+    @opportunity = @opportunity.pluck(:potential_seats).compact.inject(:+) || 0
+
+    @total = @total.joins(:company).where("companies.name NOT ILIKE ?", exclusion) unless current_user.root?
   end
 
   def opportunities
