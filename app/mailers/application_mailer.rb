0cc6ca07655d7d4a2c9a2b6f8c1df995a92e4875
Ashutosh Joshi <ashutoshj@ubuntu.(none)>
2015-11-05 10:35:39 -0800

    more verbiage tweaks

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
   default from: %("VMware EUC Demo Portal" <noreply@vmwdemo.com>)
-  default reply_to: 'eucdemohelpp@vmware.com'
+  default reply_to: 'eucdemohelp@vmware.com'
   layout 'mailer'
 end
