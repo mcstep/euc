736e490d8e55db1d356dff7417d6cf132097f9fd
Boris Staal <boris@staal.io>
2015-11-19 21:49:37 +0300

    BCC admin by default

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
   default from: %("VMware EUC Demo Portal" <noreply@vmwdemo.com>)
   default reply_to: 'eucdemohelp@vmware.com'
+  default bcc: 'admin@vmwdemo.com'
   layout 'mailer'
 end
